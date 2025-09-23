de8bd23f00ce2dbef16e9cd34d0eb08b4828ade3
Iven <1158160439@qq.com>
2025-09-24 02:52:56 +0800

    新增vercel/speed-insights

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import { HeroSection } from "@/components/ui/hero-section-1"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function Home() {
   return (
     <main>
       <HeroSection />
+      <SpeedInsights/>
     </main>
   )
 }
